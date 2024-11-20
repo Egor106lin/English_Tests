37fd5d7e99fc89da3fe61218b802b40864e5aab0
Egor Staroverov <staroverov.egor.2010@gmail.com>
2024-11-20 18:38:39 +0300

    commit number 1

---
+++ check_answer.py
@@ -0,0 +1,38 @@
+import unittest
+from words_list import words
+
+def check_answer(cookie_value_dictionary: dict, answer: list) -> dict:
+    '''This function gets cookie value and users's answer, returns cookie value
+    with 'count' +1 if the answer is correct, and returns it without changes if
+    the answer is uncorrect'''
+    count = cookie_value_dictionary['count']
+    question = cookie_value_dictionary['question']
+    for i in answer:
+        if i == None:
+            continue
+        elif i != None:
+            user_answer = answer[i]
+    values = []
+    for value in words.values():
+        values.append(value)
+    if user_answer == values[question][1]:
+        count += 1
+    cookie_value_dictionary['count'] = count
+    return cookie_value_dictionary
+
+
+class TestCookieFunctions(unittest.TestCase):
+
+    def test_check_answer(self):
+        cookie_value_dictionary = {'state': 'words', 'count': 0, 'question': 1}
+        answer = 'разность'
+        values = []
+        for value in words.values():
+            values.append(value)  
+        result = check_answer(cookie_value_dictionary, answer)
+        self.assertEqual(result, {'state': 'words', 'count': 0, 'question': 1})
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from word_exercise import context
 from words_list import words
 
 from cookie_set import from_cookie_to_dict, from_dict_to_cookie
+from check_answer import check_answer
 
 app = Flask(__name__)
-
+                    
 
 @app.route("/")
 def main_page():
@@ -16,14 +17,7 @@ def main_page():
 
 @app.route("/words/", methods=["GET", "POST"])
 def words_page():
-    context = {
-        "number": "",
-        "english_word": "",
-        "translate1": "",
-        "translate2": "",
-        "translate3": "",
-        "translate4": ""
-    }
+    # create res
     res = make_response()
     res.set_cookie('universal', "")
     # make a dictionary with cookie value
@@ -31,24 +25,43 @@ def words_page():
     cookie_value_dictionary = from_cookie_to_dict(value_of_cookie)
     # code
     if cookie_value_dictionary['state'] == 'start':
-        res = make_response(render_template('words.html'))
+        res = make_response(render_template('start.html'))
         cookie_value_dictionary = {
             'state': 'words',
             'question': 0,
             'count': 0
         }
-    elif cookie_value_dictionary["state"] == 'words':
-        if cookie_value_dictionary["question"] <= 100:
-            
+    elif cookie_value_dictionary['state'] == 'words':
+        if cookie_value_dictionary['question'] < 100:
+            context_list = []
+            answer = []
+            for key in context:
+                context_list.append(key)
+            for name in ['word1', 'word2', 'word3', 'word4']:
+                answer.append(request.form.get(name))
+            print(answer)
+            if cookie_value_dictionary['question'] != 0:
+                check_answer(cookie_value_dictionary, answer)
+            dictionary_list = context[context_list[cookie_value_dictionary['question']]]
+            exercise = {
+                'number': cookie_value_dictionary['question'] + 1,
+                'english_word': dictionary_list[0],
+                'translate1': dictionary_list[1],
+                'translate2': dictionary_list[2],
+                'translate3': dictionary_list[3],
+                'translate4': dictionary_list[4],
+            }
+            cookie_value_dictionary['question'] += 1
+            res = make_response(render_template('words.html', **exercise))
         else:
-            cookie_value_dictionary["state"] = 'finish'
-            res = make_response(render_template('finish.html'))
+            cookie_value_dictionary['state'] = 'finish'
+            res = make_response(render_template('finish.html', result=cookie_value_dictionary['count']))
     elif cookie_value_dictionary['state'] == 'finish':
-        cookie_value_dictionary["state"] = 'start'
+        cookie_value_dictionary['state'] = 'start'
+        res = make_response(render_template('start.html'))
     # make a string from dictionary
     res.set_cookie('universal', from_dict_to_cookie(cookie_value_dictionary))
     return res
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    app.run(debug=True) 
